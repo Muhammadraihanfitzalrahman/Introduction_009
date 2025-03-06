cde077fb7e08c93c0ed2ee98ccf0689d0b2f2142
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-03-06 11:24:21 +0700

    programutama dan programkedua

---
+++ Programkedua.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+float luas, panjang, lebar; //var global
+
+void inputData(){}
+cout <<"masukan panjangnya"
+cin >> panjang;
+cout  << "masukan lebarnya"
+cin >> lebar;
+ 
+
+
+int main()
+{
+    inputData();
+    //hitungluas();
+    hitungluarberapameter{panjang lebar}
+    tampilkanluas
+}
+
